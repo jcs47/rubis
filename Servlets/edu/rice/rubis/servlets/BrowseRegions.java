d07d9d19a3e409a87078fcd9866b12385c11f2a4
Joao Sousa <jcs@lasige.di.fc.ul.pt>
2016-07-07 20:27:12 +0100

    Modified query and added temporary debug messages/instructions to servlet.

--- Servlets/edu/rice/rubis/servlets/BrowseRegions.java
+++ Servlets/edu/rice/rubis/servlets/BrowseRegions.java
@@ -1,16 +1,22 @@
 package edu.rice.rubis.servlets;
 
+import bftsmart.tom.util.TOMUtil;
+import static edu.rice.rubis.servlets.RubisHttpServlet.getCache;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.Arrays;
+import java.util.LinkedList;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lasige.steeldb.jdbc.BFTPreparedStatement;
+import lasige.steeldb.jdbc.ResultSetData;
+import merkletree.MerkleTree;
 import merkletree.TreeCertificate;
+import org.json.JSONArray;
 
 /** Builds the html page with the list of all region in the database */
 public class BrowseRegions extends RubisHttpServlet
@@ -33,19 +39,21 @@ public class BrowseRegions extends RubisHttpServlet
     Connection conn = null;
     String regionName;
     ResultSet rs = null;
-
+    
+    String sql = "SELECT * FROM regions";
+    boolean fromCache = false;
+    
     // get the list of regions
     try
     {
       conn = getConnection();
-
-      String sql = "SELECT name, id FROM regions";
       
       stmt = getCache().prepareStatement(sql, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
       rs = stmt.executeQuery();
       
       if (rs.first()) {
           sp.printHTML("Successfully fetched from cache!");
+          fromCache = true;
       }
       else {
           sp.printHTML("Fetching from database");
@@ -77,6 +85,75 @@ public class BrowseRegions extends RubisHttpServlet
         sp.printRegion(regionName);
       }
       while (rs.next());
+      
+      if (fromCache) { // temporary code
+          
+          stmt = getCache().prepareStatement("SELECT name, id from regions", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+          rs = stmt.executeQuery();
+          
+          sp.printHTML("<h3>Re-creating first branches from result set fetched from cache (table 'regions')</h3>");
+          JSONArray json = TreeCertificate.getJSON((new ResultSetData(rs)).getRows());
+          //sp.printHTML("<p>JSON format: " + json + "</p>");
+          //sp.printHTML("<p>Branches:</p>");
+
+          MerkleTree[] branches = TreeCertificate.getFirstLevel(TreeCertificate.jsonToLeafs(json));
+          
+          for (MerkleTree b : branches) {
+              sp.printHTML("<p>" + org.apache.catalina.tribes.util.Arrays.toString(b.digest()) + "</p>");
+          }
+          
+          sp.printHTML("<h3>Getting first branches from cache (table 'branches')</h3>");
+          
+          stmt = getCache().prepareStatement("SELECT * from regions", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+          rs = stmt.executeQuery();
+          
+          rs.next();
+          long ts = rs.getLong("timestamp");
+          
+          stmt = getCache().prepareStatement("SELECT * from branches WHERE timestamp = " +  ts + " AND index = 0 ORDER BY position", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+          rs = stmt.executeQuery();
+          
+          rs.last();
+          branches = new MerkleTree[rs.getRow()];
+          rs.beforeFirst();
+          
+          int position = 0;
+          while (rs.next()) {
+              byte[] data = rs.getBytes("value");
+              sp.printHTML("<p>" + org.apache.catalina.tribes.util.Arrays.toString(data) + "</p>");
+              branches[position] = new MerkleTree(null, data);
+              position++;
+          }
+          
+          while (branches.length > 1)
+              branches = TreeCertificate.getNextLevel(branches);
+          
+          sp.printHTML("<h3>MerkleTree root:</h3>");
+          sp.printHTML("<p>" + org.apache.catalina.tribes.util.Arrays.toString(branches[0].digest())  + "</p>");
+          
+          sp.printHTML("<h3>Verifiyng signatures:</h3>");
+          
+          LinkedList<byte[]> l = new LinkedList<>();
+          for (MerkleTree b : branches) {
+              l.add(b.digest());
+          }
+          
+
+          stmt = getCache().prepareStatement("SELECT * FROM signatures WHERE timestamp = " + ts, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+          rs = stmt.executeQuery();
+          while (rs.next()) {
+          
+            byte[] buffer = TreeCertificate.concatenate(rs.getInt("replica"), ts, l);
+            boolean verify = TOMUtil.verifySignature(getReplicaKey(rs.getInt("replica")), buffer, rs.getBytes("value"));
+            //sp.printHTML("<p>Concatenation from replica " + rs.getInt("replica") + ": " + Arrays.toString(buffer) + "</p>");
+            
+            //sp.printHTML("<p>Signature for replica " + rs.getInt("replica") + ": " + Arrays.toString(rs.getBytes("value")) + "</p>");
+            
+            sp.printHTML("<p>Verified: " + rs.getInt("replica") + ": " + verify + "</p>");
+            
+          }
+      }
+      
       closeConnection(stmt, conn);
       
     }
